7a34a96cdcc12d91ccf37954e19a7456fae9ff87
snaxxis <snaxx@live.se>
2025-08-27 20:19:47 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-toggleBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
